0155e7e551ac941f9f0e6d465cc5440e4c492b27
JoseM <jmggalavis@gmail.com>
2020-04-17 22:41:31 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jmggalavis:tImsvlKCy4NQTbdk@cluster0-5cbvt.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
